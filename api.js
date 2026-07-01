88684524a2e1928df81b44acfdef5a87538615a6
sukhvinder kaur <ksukhvinder254@gmail.com>
2026-07-01 14:44:52 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:8000'
+  baseURL: 'https://studymind-ai-backend-kc9l.onrender.com/'
 })
 
 export const signup = (data) => API.post('/auth/signup', data)
